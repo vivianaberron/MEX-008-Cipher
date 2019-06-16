65a54ca29dac152b01883f2d1479c6758d52fdd0
Mayra Viviana Berrón Márquez <vivianaberron@gmail.com>
2019-06-16 14:17:29 -0500

    Prueba de código en cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,15 +1,38 @@
 window.cipher = {
-  encode: () => {}
-  // ... 
-  //let cipher = (string, number) => {
-   // let box = ''; //Contiene la palabra a cifrar
-    //for (let i = 0; i< string.length;i++){
-    //  let newAscii = string.charCodeAt(i);
-     // let cipherAscii = (newAscii-65+number)%26+65;
-     // let cipherLetter = (String.fromCharCode (cipherAscii));
-     // box = box + cipherLetter;
-   // }
-  //return box
- // }
- // cipher('ABC', 1)
-// };
+  encode: (string, number ) => {
+    //recibiendo mensaje
+    let cipherEncode = ''; //Contiene la palabra a cifrar
+      for (let i = 0; i< string.length;i++){
+       let newAscii = string.charCodeAt(i); //Recorriendo caracteres
+      let cipherAscii = (newAscii-65+number)%26+65; //aplicando fórmula de cifrado
+        let cipherLetter = (String.fromCharCode (cipherAscii));
+        box = box + cipherLetter;
+      }
+     return box
+    }
+
+ decode: (string, number) => {
+   //recibiendo mensaje
+     let boxDecode = ''; //Contiene la palabra a descifrar 
+        for (let i = 0; i< string.length;i++){
+          let newAsciiDecode = string.charCodeAt(i);
+           let cipherAsciiDecode = (newAscii+65-number)%26+65;
+            let cipherLetterD = (String.fromCharCode (cipherAscii));
+           boxDecode = boxDecode + cipherLetter;
+        }
+        return boxDecode
+      } 
+
+
+  
+
+
+
+
+
+
+
+
+
+  }
+  

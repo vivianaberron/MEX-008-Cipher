9dc2e81c2c0f4df51f8989d69564da3f54f0eabe
Mayra Viviana Berrón Márquez <vivianaberron@gmail.com>
2019-06-18 18:36:55 -0500

    Haciendo botón home

--- src/cipher.js
+++ src/cipher.js
@@ -2,13 +2,21 @@ window.cipher = {
   encode: (number, string ) =>{
     //recibiendo mensaje
     let cipherEncode = ''; //Contiene la palabra a cifrar
-    for (let i = 0; i < string.length ;i++){
-       let newAscii = string.charCodeAt(i); //Recorriendo caracteres
-      let cipherAscii = (newAscii-65+number)%26+65; //aplicando fórmula de cifrado
+    for (let i = 0; i < string.length ;i++){ //Recorriendo caracteres
+       let newAscii = string.charCodeAt(i); 
+       if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+        let cipherAscii = (newAscii-65+number)%26+65; //aplicando fórmula de cifrado
         let cipherLetter = (String.fromCharCode (cipherAscii));
         cipherEncode = cipherEncode + cipherLetter;
+      } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i)<=122) {
+        let cipherAscii = (newAscii-65+number)%26+65; //aplicando fórmula de cifrado
+        let cipherLetter = (String.fromCharCode (cipherAscii));
+        cipherEncode = cipherEncode + cipherLetter;
+      } else {
+        cipherEncode = newAscii[i];
       }
-     return cipherEncode
+    }
+     return cipherEncode;
     },
    
 

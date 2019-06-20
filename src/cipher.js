adc8781c6bb3c32b51bb832687612971fd2b5a69
Mayra Viviana Berrón Márquez <vivianaberron@gmail.com>
2019-06-19 21:36:44 -0500

    Agregando minúsculas y caracteres especiales a cipher

--- src/cipher.js
+++ src/cipher.js
@@ -2,18 +2,18 @@ window.cipher = {
   encode: (number, string ) =>{
     //recibiendo mensaje
     let cipherEncode = ''; //Contiene la palabra a cifrar
-    for (let i = 0; i < string.length ;i++){ //Recorriendo caracteres
+    for (let i = 0; i < string.length; i++) { //Recorriendo caracteres
        let newAscii = string.charCodeAt(i); 
-       if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
-        let cipherAscii = (newAscii-65+number)%26+65; //aplicando fórmula de cifrado
+       if (newAscii >= 65 && newAscii <= 90){
+        let cipherAscii = (newAscii - 65 + number) % 26 + 65; //aplicando fórmula de cifrado
         let cipherLetter = (String.fromCharCode (cipherAscii));
         cipherEncode = cipherEncode + cipherLetter;
-      } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i)<=122) {
-        let cipherAscii = (newAscii-65+number)%26+65; //aplicando fórmula de cifrado
+      } else if  (newAscii >= 97 && newAscii <= 122){
+        let cipherAscii = (newAscii - 97 + number) % 26 + 97; //aplicando fórmula de cifrado
         let cipherLetter = (String.fromCharCode (cipherAscii));
         cipherEncode = cipherEncode + cipherLetter;
       } else {
-        cipherEncode = newAscii[i];
+        cipherEncode = cipherEncode + newAscii[i];
       }
     }
      return cipherEncode;
@@ -24,26 +24,17 @@ window.cipher = {
    //recibiendo mensaje
      let cipherDecode = ''; //Contiene la palabra a descifrar 
         for (let i = 0; i < string.length;i++){
-          console.log(number);
-          console.log(string);
           let newAscii = string.charCodeAt(i);
-           let cipherAsciiDecode = (newAscii+65-number+52)%26+65;
+          if (newAscii >= 65 && newAscii <=90){
+           let cipherAsciiDecode = (newAscii-65-number+52)%26+65;
             let cipherLetterD = (String.fromCharCode (cipherAsciiDecode));
            cipherDecode = cipherDecode + cipherLetterD;
+          } else if (newAscii >= 67 && newAscii <=122) {
+            let cipherAsciiDecode = (newAscii-97-number+52)%26+97;
+             let cipherLetterD = (String.fromCharCode (cipherAsciiDecode));
+            cipherDecode = cipherDecode + cipherLetterD;
+           } else if 
+           (cipherDecode = cipherDecode + newAscii[i]);
         }
-        return cipherDecode
- }
-
-
-  
-
-
-
-
-
-
-
-
-
-  }
-  
+        return cipherDecode;
+      }};
